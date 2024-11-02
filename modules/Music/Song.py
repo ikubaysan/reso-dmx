e2d926c173cf92b127eca381b6b675055232bebf
ikubay <ikubaysan@gmail.com>
2024-11-01 17:14:48 -0700

    Choose last bpm if all bpms are within 1 of each other

--- modules/Music/Song.py
+++ modules/Music/Song.py
@@ -160,11 +160,11 @@ class Song:
         self.sample_start = sample_start
         self.sample_length = sample_length
 
-        # For some reason, I noticed that in old mixes like DDR 1-8th mix,
-        # some charts have 2 bpms and they're less than 1 bpm apart.
-        # In this case, we'll just use the 2nd bpm, but set its start time to 0.
-        if len(bpms) == 2 and bpms[-1][1] - bpms[0][1] < 1:
+        # Check if there’s more than 1 BPM and if all BPMs are within 1 BPM of each other
+        # If so, keep only the last BPM
+        if len(bpms) > 1 and all(abs(bpms[i][1] - bpms[i + 1][1]) < 1 for i in range(len(bpms) - 1)):
             bpms = [(0.0, bpms[-1][1])]
+
         self.bpms = bpms
 
         if len(self.bpms) == 0:
@@ -183,7 +183,8 @@ class Song:
         return
 
     @staticmethod
-    def parse_sm_file(sm_file_path: str) -> Tuple[str, str, float, float, List[Tuple[float, float]], List[Dict[str, Any]]]:
+    def parse_sm_file(sm_file_path: str) -> Tuple[
+        str, str, float, float, List[Tuple[float, float]], List[Dict[str, Any]]]:
         title = ""
         artist = ""
         bpms = []
@@ -195,67 +196,63 @@ class Song:
             in_notes_section = False
             current_mode = ""
             current_difficulty_name = ""
-            current_difficulty_level = 0
+            current_difficulty_level = None  # Set to None to detect if it’s explicitly set for each chart
             notes_data = []
-            measures = []  # List to store notes for the current measure
+            measures = []
 
             for line in sm_file:
-                line = line.strip()
-
-                line = line.strip().lstrip('\ufeff')  # Remove BOM if present
+                line = line.strip().lstrip('\ufeff')
                 line_lower = line.lower()
 
                 if line_lower.startswith("#title:"):
-                    title = line.split(":")[1].strip()
-                    title = title.rstrip(';')
+                    title = line.split(":")[1].strip().rstrip(';')
                 elif line_lower.startswith("#artist:"):
-                    artist = line.split(":")[1].strip()
-                    artist = artist.rstrip(';')
+                    artist = line.split(":")[1].strip().rstrip(';')
                 elif line_lower.startswith("#bpms:"):
-                    bpms_data = line.split(":")[1].strip()
-                    bpms_data = bpms_data.rstrip(';')
-                    # First value is the beat (integer), second value is the bpm (float, but almost always an integer)
-                    # but I made both values floats here.
+                    bpms_data = line.split(":")[1].strip().rstrip(';')
                     bpms = [tuple(map(float, bpm.split("="))) for bpm in bpms_data.split(",")]
                 elif line_lower.startswith("#notes:"):
+                    if in_notes_section and current_mode and current_difficulty_name and current_difficulty_level is not None:
+                        chart = Chart(
+                            mode=current_mode,
+                            difficulty_name=current_difficulty_name,
+                            difficulty_level=current_difficulty_level,
+                            measures=measures
+                        )
+                        charts.append(chart)
+                        measures = []
                     in_notes_section = True
+                    current_difficulty_level = None  # Reset for each chart
                 elif line_lower.startswith("#samplestart:"):
-                    # Eg. line = "#SAMPLESTART:61.34;"
-                    # Split by ':', take the second part, split by ';', and cast to float.
                     sample_start = float(line.split(':')[1].split(';')[0])
                 elif line_lower.startswith("#samplelength:"):
-                    # Eg. line = "#SAMPLELENGTH:10.00;"
-                    # Split by ':', take the second part, split by ';', and cast to float.
                     sample_length = float(line.split(':')[1].split(';')[0])
                 elif in_notes_section:
                     if line.startswith("dance-single:") or line.startswith("dance-double:"):
-                        current_mode = line.strip()  # Set the current mode
-                        # Remove the : from the end of the line
-                        current_mode = current_mode.rstrip(':')
-                    elif line.endswith(":") and len(line) > 0 and current_difficulty_name == "":
+                        current_mode = line.rstrip(':')
+                    elif line.endswith(":") and not current_difficulty_name:
                         current_difficulty_name = line.rstrip(':').strip()
-                    elif line[:-1].isdigit() and current_difficulty_level == 0:
-                        # Check if the line (excluding the last character) is a digit. Only set if it has not been set yet.
+                    elif line[:-1].isdigit() and current_difficulty_level is None:
+                        # Capture the difficulty level correctly when the line contains a digit and hasn’t been set yet
                         current_difficulty_level = int(line[:-1])
-                    elif line.startswith(","):  # End of measure. Using startswith instead of equals because there could be comments
-                        measures.append(notes_data)  # Add notes data for the current measure
-                        notes_data = []  # Reset notes data for the next measure
+                    elif line.startswith(","):
+                        measures.append(notes_data)
+                        notes_data = []
                     elif line.startswith(";"):
                         in_notes_section = False
-                        if current_mode and current_difficulty_name and current_difficulty_level:
+                        if current_mode and current_difficulty_name and current_difficulty_level is not None:
                             chart = Chart(
                                 mode=current_mode,
                                 difficulty_name=current_difficulty_name,
                                 difficulty_level=current_difficulty_level,
-                                measures=measures  # Store measures as a list
+                                measures=measures
                             )
                             charts.append(chart)
-                            current_difficulty_name = ""  # Reset difficulty name
-                            current_difficulty_level = 0  # Reset difficulty level
-                            notes_data = []  # Reset notes data
-                            measures = []  # Reset measures
+                            current_difficulty_name = ""
+                            current_difficulty_level = None
+                            notes_data = []
+                            measures = []
                     elif line.isnumeric():
-                        # Capture the notes data, which looks like 0000
                         notes_data.append(line)
 
         return title, artist, sample_start, sample_length, bpms, charts
